e5899d1bf468af287f7a6c687c748fdead7e02eb
SmiteMeSmith <smitemesmith@gmail.com>
2015-10-22 23:48:07 -0400

    Changed max number from 15 to 256 so that all 8 LEDs are used and divided the time delay by 4

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 
 //Delay function waste time counting. 32767 is largest positive 16 bit 2CN number
 void delay() {      
-    int delaycount = 0;   
-    for (delaycount = 0; delaycount<32767; delaycount++);  //works with "RUN"  
+    int delaycount = 0;
+    for (delaycount = 0; delaycount<(32767/4); delaycount++);  //works with "RUN"  
     //for (delaycount = 0; delaycount<2; delaycount++);  	   //works with "STEP INTO"  
  }   
 
@@ -14,20 +14,20 @@ void delay() {
 void main(void) {      
     int counter = 0;   
     TRISJ = 0xff;	// all bits of portJ are set 1 as input      
-    TRISE = 0; 		// all bits of portE are set 0 as output      
-    PORTE = 0; 		// turn off all led's   
+    TRISE = 0x00; 		// all bits of portE are set 0 as output      
+    PORTE = 0x00; 		// turn off all led's   
 
 //while PORTJ switch #7 = 1 you execute the "if/else" code that uses 
 //  switch #0 to control count direction
     while (PORTJbits.RJ7) {          
        if (PORTJbits.RJ0 == 0) { 				//if switch =0 count forward                          
-            for (counter = 0; counter<=15; counter++)
+            for (counter = 0; counter<=256; counter++)
 			{
 				PORTE = counter; 
 			 	delay();
 			} 
 	   } else {              					//else count backwards
-            for (counter = 15; counter>-1; counter--)
+            for (counter = 256; counter>-1; counter--)
 			{
 				PORTE = counter; 
 			 	delay();
